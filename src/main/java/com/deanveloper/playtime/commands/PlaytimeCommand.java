9217be410bc511e01c7fc387110aa356aa22853d
Dean B <deanvelopermn@gmail.com>
2016-08-27 10:16:54 -0400

    Changed how the command works, and added a way to change player's playtimes

--- src/main/java/com/deanveloper/playtime/commands/PlaytimeCommand.java
+++ src/main/java/com/deanveloper/playtime/commands/PlaytimeCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -19,20 +20,55 @@ import java.util.stream.Collectors;
 public class PlaytimeCommand implements CommandExecutor, TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
-        if (sender.hasPermission("playtime.command.playtime") && args.length > 0 && !args[0].equalsIgnoreCase("top")) {
-            UUID id = Utils.getUuid(args[0]);
-            if (id == null) {
-                sender.sendMessage("Couldn't find player " + args[0]);
-            } else {
-                int time = PlayTime.getPlayerDb().get(id.toString(), 0);
-                sender.sendMessage(
-                        String.format("§a[Playtime] §d%s §ehas played for §d%s§e.",
-                                Utils.correctCase(args[0]), Utils.format(time))
-                );
-            }
-        } else if (sender instanceof Player) {
-            try {
-                if (args.length > 0 && args[0].equalsIgnoreCase("top")) {
+        if (args.length == 0) {
+            return onCommand(sender, cmd, lbl, new String[]{"self"});
+        } else if (args.length >= 1) {
+            args[0] = args[0].toLowerCase();
+
+            // HELP
+            if (args[0].equals("help")) {
+                StringJoiner joiner = new StringJoiner("|", "[", "]");
+                joiner.add("help");
+                if (sender.hasPermission("playtime.command.playtime.self")) {
+                    joiner.add("self");
+                }
+                if (sender.hasPermission("playtime.command.playtime.top")) {
+                    joiner.add("top");
+                }
+                if (sender.hasPermission("playtime.command.playtime.change")) {
+                    joiner.add("change");
+                }
+                if (sender.hasPermission("playtime.command.playtime.other")) {
+                    joiner.add("playername");
+                }
+
+                if (joiner.length() == 0) {
+                    sender.sendMessage("You do not have permission to use this command!");
+                } else {
+                    sender.sendMessage("Usage: " + joiner.toString());
+                }
+
+                // SELF
+            } else if (args[0].equals("self")) {
+                if (!sender.hasPermission("playtime.command.playtime.self")) {
+                    sender.sendMessage("§cYou don't have permission to view your playtime");
+                } else {
+                    if (sender instanceof Player) {
+                        int time = PlayTime.getPlayerDb().get(((Player) sender).getUniqueId().toString(), 0);
+
+                        sender.sendMessage("§a[Playtime] §dYou §ehave played for §d"
+                                + Utils.format(time) + "§e.");
+                    } else {
+                        sender.sendMessage("You need to be a player to see your playtime, silly!");
+                    }
+                }
+
+                // TOP
+            } else if (args[0].equals("top")) {
+                if (!sender.hasPermission("playtime.command.playtime.top")) {
+                    sender.sendMessage("§cYou don't have permission to view the top players");
+                }
+                try {
                     List<String> topTenIds = new ArrayList<>(PlayTime.getPlayerDb().getConfig().getKeys(false));
                     //sort from most to least
                     topTenIds = topTenIds.stream()
@@ -67,16 +103,61 @@ public class PlaytimeCommand implements CommandExecutor, TabExecutor {
                                 )
                         );
                     }
-                } else {
-                    int time = PlayTime.getPlayerDb().get(((Player) sender).getUniqueId().toString(), 0);
-
-                    sender.sendMessage("§a[Playtime] §dYou §ehave played for §d"
-                            + Utils.format(time) + "§e.");
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                    sender.sendMessage("Oops... The developer made a boo-boo: " + e.getMessage());
+                    sender.sendMessage("Tell the server owner to send the full error in their console to the developer!");
                 }
-            } catch (NullPointerException e) {
-                e.printStackTrace();
-                sender.sendMessage("Oops... The developer made a boo-boo: " + e.getMessage());
-                sender.sendMessage("Tell the server owner to send the full error in their console to the developer!");
+
+                // CHANGE
+            } else if (args[0].equals("change")) {
+                if (!sender.hasPermission("playtime.command.playtime.change")) {
+                    sender.sendMessage("§cYou do not have permission to change people's playtimes");
+                }
+                if (args.length >= 3) {
+                    UUID id = Utils.getUuid(args[1]);
+                    if (id == null) {
+                        sender.sendMessage("§cCouldn't find player " + args[1]);
+                    } else {
+                        if (args[2].startsWith("+") || args[2].startsWith("-")) {
+                            try {
+                                int change = Integer.parseInt(args[2]);
+
+                                int playTime = PlayTime.getPlayerDb().get(id.toString(), 0);
+                                playTime += change;
+                                PlayTime.getPlayerDb().set(id.toString(), playTime);
+
+                                sender.sendMessage(args[1] + "'s playtime was changed by " + change + " seconds");
+                                sender.sendMessage("§7Their playtime is now at " + Utils.format(playTime));
+                            } catch (NumberFormatException e) {
+                                return onCommand(sender, cmd, lbl, new String[] {"change"});
+                            }
+                        } else {
+                            return onCommand(sender, cmd, lbl, new String[] {"change"});
+                        }
+                    }
+                } else {
+                    sender.sendMessage("Usage: /playtime change <playername> <(+|-)seconds>");
+                }
+
+                // OTHER
+            } else {
+                if(!sender.hasPermission("playtime.command.playtime.other")) {
+                    sender.sendMessage("§cYou do not have permission to view other people's playtimes!");
+                } else {
+                    if(args.length >= 1) {
+                        UUID id = Utils.getUuid(args[0]);
+                        if (id == null) {
+                            sender.sendMessage("Couldn't find player " + args[0]);
+                        } else {
+                            int time = PlayTime.getPlayerDb().get(id.toString(), 0);
+                            sender.sendMessage(
+                                    String.format("§a[Playtime] §d%s §ehas played for §d%s§e.",
+                                            Utils.correctCase(args[0]), Utils.format(time))
+                            );
+                        }
+                    }
+                }
             }
         }
         return true;
