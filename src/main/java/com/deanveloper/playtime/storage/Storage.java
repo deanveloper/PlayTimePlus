f87f846a8a79408adf758b48352682de65a3079a
Dean B <dean@deanveloper.com>
2016-09-20 19:58:08 -0400

    Added documentation

--- src/main/java/com/deanveloper/playtime/storage/Storage.java
+++ src/main/java/com/deanveloper/playtime/storage/Storage.java
@@ -7,14 +7,31 @@ import java.util.*;
  */
 public interface Storage {
 
+    /**
+     * Creates a PlayerEntry object as well as loading it into the local cache.
+     * Implementation should do the checking to see if the PlayerEntry is already present.
+     */
     void createIfNotPresent(UUID id);
 
+    /**
+     * Saves local cache to permanent storage.
+     */
     void save();
 
+    /**
+     * Updates a player entry into the local cache.
+     * Implementation should do nothing if the entries themselves are stored locally.
+     */
     void update(PlayerEntry entry);
 
+    /**
+     * Gets the PlayerEntry for the associated UUID.
+     */
     PlayerEntry get(UUID id);
 
+    /**
+     * Gets all players who have PlayerEntries.
+     */
     Map<UUID, PlayerEntry> getPlayers();
 
 }
