5ff45d551295af8d0848edae189a15cb72ceebe8
Dean B <dean@deanveloper.com>
2016-09-28 01:28:10 -0400

    Huge performance improvement for RAM since we aren't creating tons of PlayerEntries and TimeEntries! (blame gson for all that crap)

--- src/main/java/com/deanveloper/playtimeplus/storage/JsonStorage.java
+++ src/main/java/com/deanveloper/playtimeplus/storage/JsonStorage.java
@@ -5,10 +5,12 @@ import com.google.common.io.Files;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.UUID;
@@ -19,48 +21,41 @@ import java.util.stream.Collectors;
  */
 public class JsonStorage implements Storage {
     private final File storage;
-    private final JsonObject root;
-    private final JsonElement version;
-    private final JsonObject players;
+    private final Map<UUID, PlayerEntry> players;
+    private final int version;
 
     JsonStorage() {
         storage = new File(PlayTimePlus.getInstance().getDataFolder(), "players.json");
-        JsonObject temp;
+
+        // Parse the file
+        JsonObject root;
         try {
             String line = Files.readFirstLine(storage, Charset.defaultCharset());
             if (line == null || line.isEmpty()) {
                 throw new FileNotFoundException("Just in case");
             }
-            temp = new JsonParser().parse(line).getAsJsonObject();
+            root = new JsonParser().parse(line).getAsJsonObject();
         } catch (FileNotFoundException e) {
-            temp = new JsonObject();
-            temp.addProperty("version", 1);
-            temp.add("players", new JsonObject());
+            root = new JsonObject();
+            root.addProperty("version", 1);
+            root.add("players", new JsonObject());
             save();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        root = temp;
-        version = root.get("version");
-        players = root.getAsJsonObject("players");
-    }
 
-    public JsonObject getRoot() {
-        return root;
-    }
-
-    @Override
-    public void createIfNotPresent(UUID id) {
-        if(players.get(id.toString()) != null) {
-            return;
-        }
-
-        players.add(id.toString(), PlayTimePlus.GSON.toJsonTree(new PlayerEntry(id)));
+        // Now let's get our fields
+        version = root.get("version").getAsInt();
+        Type type = new TypeToken<Map<UUID, PlayerEntry>>(){}.getType();
+        players = PlayTimePlus.GSON.fromJson(root.getAsJsonObject("players"), type);
     }
 
     @Override
     public void save() {
         try {
+            JsonObject root = new JsonObject();
+            root.addProperty("version", version);
+            root.add("players", PlayTimePlus.GSON.toJsonTree(players));
             Files.write(PlayTimePlus.GSON.toJson(root), storage, Charset.defaultCharset());
         } catch (IOException e) {
             e.printStackTrace();
@@ -68,23 +63,12 @@ public class JsonStorage implements Storage {
     }
 
     @Override
-    public void update(PlayerEntry entry) {
-        players.add(entry.getId().toString(), PlayTimePlus.GSON.toJsonTree(entry));
-    }
-
-    @Override
     public PlayerEntry get(UUID id) {
-        return PlayTimePlus.GSON.fromJson(players.get(id.toString()), PlayerEntry.class);
+        return getPlayers().get(id);
     }
 
     @Override
     public Map<UUID, PlayerEntry> getPlayers() {
-        synchronized (players) {
-            return players.entrySet().parallelStream()
-                    .collect(Collectors.toMap(
-                            entry -> UUID.fromString(entry.getKey()),
-                            entry -> PlayTimePlus.GSON.fromJson(entry.getValue(), PlayerEntry.class)
-                    ));
-        }
+        return players;
     }
 }
--- src/main/java/com/deanveloper/playtimeplus/storage/Storage.java
+++ src/main/java/com/deanveloper/playtimeplus/storage/Storage.java
@@ -9,21 +9,9 @@ import java.util.function.Predicate;
 public interface Storage {
 
     /**
-     * Creates a PlayerEntry object as well as loading it into the local cache.
-     * Implementation should do the checking to see if the PlayerEntry is already present.
-     */
-    void createIfNotPresent(UUID id);
-
-    /**
      * Saves local cache to permanent storage.
      */
     void save();
-
-    /**
-     * Updates a player entry into the local cache.
-     * Implementation should do nothing if the entries themselves are stored locally.
-     */
-    void update(PlayerEntry entry);
 
     /**
      * Gets the PlayerEntry for the associated UUID.
